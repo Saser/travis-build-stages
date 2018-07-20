197cf3da6a6da6a65ef3e41610ca0ba88013f281
Christian Persson <saser@live.se>
2018-07-20 10:15:13 +0200

    Read `HERP` environment variable

--- test.js
+++ test.js
@@ -1,1 +1,2 @@
 console.log("Here I am, running the second test");
+console.log("This is the value of the HERP environment variable: " + process.env.HERP);
--- test.rb
+++ test.rb
@@ -1,1 +1,2 @@
 puts "Here I am, running the first test"
+puts "This is the value of the HERP environment variable: #{ENV['HERP']}"
